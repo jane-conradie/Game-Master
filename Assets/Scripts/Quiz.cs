3bf59a737680a11f80a965acb5e1b2f018732939
jane_conradie <jconradie1998@gmail.com>
2024-06-06 13:04:06 +0200

    adding end screen

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -32,10 +32,17 @@ public class Quiz : MonoBehaviour
     [SerializeField] TextMeshProUGUI scoreText;
     ScoreKeeper scoreKeeper;
 
+    [Header("Progress")]
+    [SerializeField] Slider progressBar;
+
+    public bool isComplete;
+
     void Start()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        progressBar.maxValue = questions.Count;
+        progressBar.value = 0;
     }
 
     void Update()
@@ -64,6 +71,7 @@ public class Quiz : MonoBehaviour
             setDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            progressBar.value++;
             scoreKeeper.IncrementQuestionsSeen();
         }
     }
@@ -107,6 +115,11 @@ public class Quiz : MonoBehaviour
         SetButtonState(false);
         timer.ResetTimer();
         scoreText.text = "score: " + scoreKeeper.CalculateScore() + "%";
+
+        if (progressBar.value == progressBar.maxValue)
+        {
+            isComplete = true;
+        }
     }
 
     void DisplayAnswer(int index)
